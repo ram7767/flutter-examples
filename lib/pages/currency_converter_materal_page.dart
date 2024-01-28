376ead7000088f5a0b06674596e33a1382856f16
ram7767 <rrnram998539@gmail.com>
2024-01-28 23:44:18 +0530

    Added the logic

--- lib/pages/currency_converter_materal_page.dart
+++ lib/pages/currency_converter_materal_page.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/material.dart';
 
-class HomeMateralPage extends StatelessWidget {
-  const HomeMateralPage({super.key});
+class HomeMateralPageStateFul extends StatefulWidget {
+  const HomeMateralPageStateFul({super.key});
+
+  @override
+  State<HomeMateralPageStateFul> createState() => _HomeMateralPageStateFul();
+}
+
+class _HomeMateralPageStateFul extends State<HomeMateralPageStateFul> {
+
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 172, 225, 212),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("hi",
-            style: TextStyle(
-              fontSize: 45,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Currency Converter",
+        style: TextStyle(fontWeight: FontWeight.normal,fontSize: 45),),
+        backgroundColor: const Color.fromARGB(255, 97, 237, 202),
+      ),
+      backgroundColor: const Color.fromARGB(255, 172, 225, 212),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Convert ${result.toStringAsFixed(3)}\$",
+              style: const TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+             const TextField(
                 style: TextStyle(
                   color: Color.fromARGB(255, 54, 153, 233),
                 ),
@@ -37,22 +51,36 @@ class HomeMateralPage extends StatelessWidget {
                     color:  Color.fromARGB(255, 251, 251, 251),
                     width: 0,
                   ),
+                  borderRadius: BorderRadius.all(Radius.elliptical(20, 20))
                  ),
                 enabledBorder: UnderlineInputBorder(
                   borderSide: BorderSide(
                     color:  Color.fromARGB(255, 251, 251, 251),
                     width: 0,
                   ),
+                  borderRadius: BorderRadius.all(Radius.elliptical(20, 20)),
                 ),
                 ),
                 keyboardType: TextInputType.numberWithOptions(
                   decimal: true,
                 ),
               ),
-            ),
-        ],
-      ),
+              const SizedBox(height: 10,),
+              ElevatedButton(
+                onPressed:  () {
+                result = double.parse(textEditingController.text) * 81;
+                setState(() {});
+                },
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.black),
+                foregroundColor: MaterialStatePropertyAll(Colors.white),
+                minimumSize: MaterialStatePropertyAll(Size(double.infinity, 50),),
+              ),
+              child: const Text("Convert"),),
+          ],
+        ),
+        ),
       ),
     );
-  }  
+  }
 }
