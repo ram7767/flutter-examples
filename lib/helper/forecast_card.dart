6ee0639c6e834827ed670f0af43ff5666c4c7252
ram7767 <rrnram998539@gmail.com>
2024-01-30 00:00:11 +0530

    Completed half UI

---
+++ lib/helper/forecast_card.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class ForecastCard extends StatelessWidget {
+  const ForecastCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+              elevation: 10,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.elliptical(15, 15))
+                ),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.elliptical(15, 15)),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(
+                      sigmaX: 10,
+                      sigmaY: 10
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text("300`F",
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                          Icon(Icons.cloud, size: 40,),
+                          Text("Rain",
+                          style: TextStyle(fontSize: 20),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+  }
+}
---
+++ lib/screen/weather_screen.dart
@@ -0,0 +1,79 @@
+import 'package:basic_weather_app/helper/forecast_card.dart';
+import 'package:basic_weather_app/helper/addtion_information_widget.dart';
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Weather App",
+        style: TextStyle(fontWeight: FontWeight.bold),),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh)),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              width: double.infinity,
+              child: ForecastCard(),
+            ),
+            SizedBox(height: 20,),
+            Text(
+              "Weather Forecast",
+              style: TextStyle(),),
+
+             SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+               child: Row(
+                children: [
+                  Card(
+                    elevation: 6,
+                    child: Container(
+                      width: 100,
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15)
+                      ),
+                      child: const Column(
+                        children: [
+                          Text("00:00",
+                          style: TextStyle(fontSize: 16),),
+                          SizedBox(height: 8,),
+                          Icon(Icons.cloud),
+                          SizedBox(height: 8,),
+                          Text("320"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+                           ),
+             ),
+
+            const SizedBox(height: 20,),
+            
+            Text("Additional Information",
+            style: TextStyle(),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                AddtionInformationWidget()
+              ],
+            ),
+            const SizedBox(height: 20,),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
