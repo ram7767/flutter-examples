7f44fbddb63d7c114eaec6d1b1de68fcc3f684b9
ram7767 <rrnram998539@gmail.com>
2024-02-01 23:39:00 +0530

    Added comments

--- lib/helper/addtion_information_widget.dart
+++ lib/helper/addtion_information_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// AddtionInformationWidget
 class AddtionInformationWidget extends StatelessWidget {
   const AddtionInformationWidget({
     super.key,
@@ -9,20 +10,29 @@ class AddtionInformationWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        Icon(Icons.water_drop_sharp,
-        size: 32,),
-        SizedBox(height: 8,),
-        Text("weather",
-        style: TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.normal,
-        ),),
-        SizedBox(height: 8,),
-        Text("16",
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
+        Icon(
+          Icons.water_drop_sharp,
+          size: 32,
         ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          "weather",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Text(
+          "16",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ],
     );
--- lib/screen/weather_screen.dart
+++ lib/screen/weather_screen.dart
@@ -2,6 +2,7 @@ import 'package:basic_weather_app/helper/forecast_card.dart';
 import 'package:basic_weather_app/helper/addtion_information_widget.dart';
 import 'package:flutter/material.dart';
 
+// Main Screen
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
 
@@ -9,8 +10,10 @@ class WeatherScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Weather App",
-        style: TextStyle(fontWeight: FontWeight.bold),),
+        title: const Text(
+          "Weather App",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
         actions: [
           IconButton(onPressed: () {}, icon: const Icon(Icons.refresh)),
@@ -25,14 +28,16 @@ class WeatherScreen extends StatelessWidget {
               width: double.infinity,
               child: ForecastCard(),
             ),
-            const SizedBox(height: 20,),
+            const SizedBox(
+              height: 20,
+            ),
             const Text(
               "Weather Forecast",
-              style: TextStyle(),),
-
-             SingleChildScrollView(
+              style: TextStyle(),
+            ),
+            SingleChildScrollView(
               scrollDirection: Axis.horizontal,
-               child: Row(
+              child: Row(
                 children: [
                   Card(
                     elevation: 6,
@@ -40,41 +45,50 @@ class WeatherScreen extends StatelessWidget {
                       width: 100,
                       padding: const EdgeInsets.all(8.0),
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15)
+                        borderRadius: BorderRadius.circular(15),
                       ),
                       child: const Column(
                         children: [
-                          Text("00:00",
-                          style: TextStyle(fontSize: 16),),
-                          SizedBox(height: 8,),
+                          Text(
+                            "00:00",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
                           Icon(Icons.cloud),
-                          SizedBox(height: 8,),
+                          SizedBox(
+                            height: 8,
+                          ),
                           Text("320"),
                         ],
                       ),
                     ),
                   ),
                 ],
-                           ),
-             ),
-
-            const SizedBox(height: 20,),
-            const Text("Additional Information",
-            style: TextStyle(),),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Additional Information",
+              style: TextStyle(),
+            ),
             const Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 AddtionInformationWidget(),
-                 AddtionInformationWidget(),
-                  AddtionInformationWidget()
+                AddtionInformationWidget(),
+                AddtionInformationWidget()
               ],
             ),
-            const SizedBox(height: 20,),
+            const SizedBox(
+              height: 20,
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
